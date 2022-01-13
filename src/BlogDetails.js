f78afdcc0848702c3215fd3fdb797bbf9ae199df
Paulina <paulina.05.krol@gmail.com>
2022-01-13 09:11:07 +0100

    finished all

---
+++ src/BlogDetails.js
@@ -0,0 +1,39 @@
+import { useHistory, useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+
+  const handleClick = (id) => {
+    // fetch(`http://localhost:8000/blogs/${id}` lub
+
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={() => handleClick(blog.id)}>Delete</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
---
+++ src/BlogList.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+const BlogList = ({ blogs, title }) => {
+  // const blogs = props.blogs;
+  // const title = props.title;
+
+  // console.log(blogs, props);
+
+  // Destrukturyzacja patrz na linie nr 1
+  // const { blogs, title } = props;
+  // console.log(blogs, title);
+
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
